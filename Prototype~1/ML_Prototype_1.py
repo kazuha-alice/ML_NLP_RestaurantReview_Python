1c38f8b930af4b4e3d53a9421a9b91271a4999d3
Shiwam Shorya Sharma <110902859+kazuha-alice@users.noreply.github.com>
2023-10-29 20:39:02 +0530

    Add files via upload

---
+++ Prototype~1/ML_Prototype_1.py
@@ -0,0 +1,7 @@
+# Importing Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('C:\ML_Data_Sets\Restaurant_Reviews.tsv', delimiter='\t',quoting = 3)
+print(dataset)
+
